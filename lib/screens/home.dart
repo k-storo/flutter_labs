b70b3204e3ead1da246c738cf9841fa154cecf69
Kostya Storozhuk <kostyastorozhuk@gmail.com>
2020-12-20 21:38:44 +0200

    added button state

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,11 +8,11 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _getAppBar(),
-      body: _getMainPage(),
-      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: _getFloatingButton(),
-    );
+        appBar: _getAppBar(),
+        body: _getMainPage(),
+        //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: MyClickedButton() //MyClickedButton(),
+        );
   }
 }
 
@@ -151,6 +151,36 @@ FloatingActionButton _getFloatingButton() {
     onPressed: () {},
     shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(0.0)),
   );
+}
+
+class MyClickedButton extends StatefulWidget {
+  MyClickedButton({Key key, this.onPressed}) : super(key: key);
+
+  // allow the widget that renders this widget to pass
+  // in a callback for when the button is pressed
+  final Function() onPressed;
+
+  @override
+  MyClickedButtonState createState() => MyClickedButtonState();
+}
+
+class MyClickedButtonState extends State<MyClickedButton> {
+  bool pressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      icon: pressed ? Icon(Icons.monetization_on) : Icon(Icons.military_tech),
+      label: pressed ? Text('ОФОРМИТЬ ПРЕМИУМ') : Text('ПРЕМИУМ ПОЛЬЗОВАТЕЛЬ'),
+      backgroundColor: pressed ? Colors.orangeAccent : Colors.green,
+      onPressed: () {
+        setState(() => {pressed = !pressed});
+        // call the callback that was passed in from the parent widget
+        widget.onPressed();
+      },
+      shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(0.0)),
+    );
+  }
 }
 
 Widget horizontalListView(title, titles, images) {
